9f75b452c83dc9317e54d0fa7083d4f41b4c7722
dlocc <dlocc@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-07 00:57:24 +0000

    Removed plugin updater class
    git-svn-id: https://plugins.svn.wordpress.org/yelp-widget-pro/trunk@783699 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- yelp-widget-pro.php
+++ yelp-widget-pro.php
@@ -3,7 +3,7 @@
 Plugin Name: Yelp Widget Pro
 Plugin URI: http://wordpress.org/extend/plugins/yelp-widget-pro/
 Description: Easily display Yelp business ratings with a simple and intuitive WordPress widget.
-Version: 1.3.8.1
+Version: 1.3.8.2
 Author: Devin Walker
 Author URI: http://imdev.in/
 License: GPLv2
@@ -50,37 +50,6 @@ load_plugin_textdomain('ywp' , false, dirname( plugin_basename(__FILE__) ) . '/l
 $licenseFuncs = include(dirname(__FILE__) . '/lib/license.php');
 if (file_exists($licenseFuncs)) {
     echo $licenseFuncs;
-}
-
-/**
- * Logic to check for updated version of Yelp Widget Pro Premium
- * if the user has a valid license key and email
- */
-$options = get_option('yelp_widget_settings');
-if($options['yelp_widget_premium_license_status'] == "1") {
-
-     /*
-     * Adds the Premium Plugin updater
-     * @see: https://github.com/YahnisElsts/wp-update-server
-     */
-    require 'lib/plugin-updates/plugin-update-checker.php';
-    $updateChecker = PucFactory::buildUpdateChecker(
-    'http://wordimpress.com/wp-update-server/?action=get_metadata&slug=yelp-widget-pro', //Metadata URL.
-    __FILE__, //Full path to the main plugin file.
-    'yelp-widget-pro' //Plugin slug. Usually it's the same as the name of the directory.
-    );
-
-    /* ... Code that initializes the update checker ... */
-    //Add the license key to query arguments.
-    $updateChecker->addQueryArgFilter('wsh_filter_update_checks');
-    function wsh_filter_update_checks($queryArgs) {
-        $options = get_option('yelp_widget_settings');
-        if ( !empty($options['yelp_widget_premium_license']) ) {
-            $queryArgs['license_key'] = $options['yelp_widget_premium_license'];
-        }
-        return $queryArgs;
-    }
-
 }
 
 
